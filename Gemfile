c92a38cc5b40ce8940e0466cccc92e339295399d
shawniscool <xiangpan@Jun-Zhengs-MacBook-Pro.local>
2015-04-12 04:10:01 -0400

    no more repeat. removed precompiled files

--- Gemfile
+++ Gemfile
@@ -30,5 +30,5 @@ end
 
 group :production do
   gem 'pg',             '0.17.1'
-  gem 'rails_12factor', '0.0.2'
+  gem 'rails_12factor', group: :production
 end
--- public/assets/application-277552ccffc87ba984c05a2f26df8780.js
+++ public/assets/application-277552ccffc87ba984c05a2f26df8780.js
@@ -13682,110 +13682,6 @@ if (typeof define == TYPE_FUNCTION && define.amd) {
 }
 
 })(window);
-(function() {
-
-
-}).call(this);
-$(document).ready(function(){
-	// $('h1').addClass('aaa');
-	// alert("start");
-	var myElement = document.getElementById('myElement');
-
-	// create a simple instance
-	// by default, it only adds horizontal recognizers
-	// listen to events...
-	// emergency.add(new Hammer.Pan({direction: Hammer.DIRECTION_ALL }));
-	// emergency.add(new Hammer.Pan({direction: Hammer.DIRECTION_LEFT }));
-
-
-
-
-	// old workable verion
-	var first = new Hammer(myElement);
-	first.add( new Hammer.Pan({ event:"panleft",direction: Hammer.DIRECTION_LEFT, threshold: 0 }) );
-	first.add( new Hammer.Pan({ event:"pandown",direction: Hammer.DIRECTION_DOWN, threshold: 0 }) );
-	first.add( new Hammer.Tap({ event: 'tap', taps: 1 }) );
-	var counter;
-	// function panLeft(event){
-	// 	console.log(event.type);
-	// 	myElement.textContent = "left pan" +" gesture detected.";
-	// 	first.on("pandown",panDown);
-	// }
-
-	// function panDown(event){
-	// 	console.log(event.type);
-	// 	counter = 10;
-	// 	myElement.textContent = "left pan and down pan" +" gesture detected.";
-	// 	first.on("tap",emergency);
-	// }
-
-	// function emergency(event){
-	// 	console.log(event.type);
-	// 	myElement.textContent = "left pan, down pan and tap" +" gesture detected.";
-	// 	if (counter == 10){
-	// 		console.log("10 here!!!!");
-	// 	}else{
-	// 		counter++;
-	// 		console.log(counter);
-	// 	}
-	// 	$.post('/alerts#create',
-	// 		{alert:{address:"500 College Ave",city:"Swarthmore", state:"PA", content:"Emergency help!"} },
-	// 		function(){
-	// 			alert("create a new object");
-	// 		});
-	// }
-
-	first.on("panleft pandown tap", check);
-	counter = 0;
-	function check(event){
-		// console.log(event.type);
-		if (event.type == "panleft" && counter == 0){
-			myElement.textContent = "left pan" +" gesture detected.";
-			counter = 5;
-		}else if (event.type == "pandown" && counter ==5){
-			myElement.textContent = "left pan" +"and down pan gesture detected.";
-			counter = 10;
-		}else if (event.type =="tap" && counter ==10){
-			myElement.textContent = "left, down pan and tap gestures are detected.";
-			counter = 20;
-
-			// connects with a model object in the backend
-			$.post('/alerts#create',
-				{alert:{address:"500 College Ave",city:"Swarthmore", state:"PA", content:"Emergency help!"} },
-				function(){
-					alert("Your message is sent");
-			});
-
-			// connects with an action method in message controller and it doesn't need a view
-			$.ajax({type:'GET',
-					url:'/sendMessage',
-					// remote:"true",
-					// have remote true so that i don't have to create a new view
-					success:function(){
-						console.log("ajax successfully submitted");
-					}
-			});
-
-		}
-	}
-
-
-
-
-});
-/*! Backstretch - v2.0.4 - 2013-06-19
-* http://srobbin.com/jquery-plugins/backstretch/
-* Copyright (c) 2013 Scott Robbin; Licensed MIT */
-
-(function(a,d,p){a.fn.backstretch=function(c,b){(c===p||0===c.length)&&a.error("No images were supplied for Backstretch");0===a(d).scrollTop()&&d.scrollTo(0,0);return this.each(function(){var d=a(this),g=d.data("backstretch");if(g){if("string"==typeof c&&"function"==typeof g[c]){g[c](b);return}b=a.extend(g.options,b);g.destroy(!0)}g=new q(this,c,b);d.data("backstretch",g)})};a.backstretch=function(c,b){return a("body").backstretch(c,b).data("backstretch")};a.expr[":"].backstretch=function(c){return a(c).data("backstretch")!==p};a.fn.backstretch.defaults={centeredX:!0,centeredY:!0,duration:5E3,fade:0};var r={left:0,top:0,overflow:"hidden",margin:0,padding:0,height:"100%",width:"100%",zIndex:-999999},s={position:"absolute",display:"none",margin:0,padding:0,border:"none",width:"auto",height:"auto",maxHeight:"none",maxWidth:"none",zIndex:-999999},q=function(c,b,e){this.options=a.extend({},a.fn.backstretch.defaults,e||{});this.images=a.isArray(b)?b:[b];a.each(this.images,function(){a("<img />")[0].src=this});this.isBody=c===document.body;this.$container=a(c);this.$root=this.isBody?l?a(d):a(document):this.$container;c=this.$container.children(".backstretch").first();this.$wrap=c.length?c:a('<div class="backstretch"></div>').css(r).appendTo(this.$container);this.isBody||(c=this.$container.css("position"),b=this.$container.css("zIndex"),this.$container.css({position:"static"===c?"relative":c,zIndex:"auto"===b?0:b,background:"none"}),this.$wrap.css({zIndex:-999998}));this.$wrap.css({position:this.isBody&&l?"fixed":"absolute"});this.index=0;this.show(this.index);a(d).on("resize.backstretch",a.proxy(this.resize,this)).on("orientationchange.backstretch",a.proxy(function(){this.isBody&&0===d.pageYOffset&&(d.scrollTo(0,1),this.resize())},this))};q.prototype={resize:function(){try{var a={left:0,top:0},b=this.isBody?this.$root.width():this.$root.innerWidth(),e=b,g=this.isBody?d.innerHeight?d.innerHeight:this.$root.height():this.$root.innerHeight(),j=e/this.$img.data("ratio"),f;j>=g?(f=(j-g)/2,this.options.centeredY&&(a.top="-"+f+"px")):(j=g,e=j*this.$img.data("ratio"),f=(e-b)/2,this.options.centeredX&&(a.left="-"+f+"px"));this.$wrap.css({width:b,height:g}).find("img:not(.deleteable)").css({width:e,height:j}).css(a)}catch(h){}return this},show:function(c){if(!(Math.abs(c)>this.images.length-1)){var b=this,e=b.$wrap.find("img").addClass("deleteable"),d={relatedTarget:b.$container[0]};b.$container.trigger(a.Event("backstretch.before",d),[b,c]);this.index=c;clearInterval(b.interval);b.$img=a("<img />").css(s).bind("load",function(f){var h=this.width||a(f.target).width();f=this.height||a(f.target).height();a(this).data("ratio",h/f);a(this).fadeIn(b.options.speed||b.options.fade,function(){e.remove();b.paused||b.cycle();a(["after","show"]).each(function(){b.$container.trigger(a.Event("backstretch."+this,d),[b,c])})});b.resize()}).appendTo(b.$wrap);b.$img.attr("src",b.images[c]);return b}},next:function(){return this.show(this.index<this.images.length-1?this.index+1:0)},prev:function(){return this.show(0===this.index?this.images.length-1:this.index-1)},pause:function(){this.paused=!0;return this},resume:function(){this.paused=!1;this.next();return this},cycle:function(){1<this.images.length&&(clearInterval(this.interval),this.interval=setInterval(a.proxy(function(){this.paused||this.next()},this),this.options.duration));return this},destroy:function(c){a(d).off("resize.backstretch orientationchange.backstretch");clearInterval(this.interval);c||this.$wrap.remove();this.$container.removeData("backstretch")}};var l,f=navigator.userAgent,m=navigator.platform,e=f.match(/AppleWebKit\/([0-9]+)/),e=!!e&&e[1],h=f.match(/Fennec\/([0-9]+)/),h=!!h&&h[1],n=f.match(/Opera Mobi\/([0-9]+)/),t=!!n&&n[1],k=f.match(/MSIE ([0-9]+)/),k=!!k&&k[1];l=!((-1<m.indexOf("iPhone")||-1<m.indexOf("iPad")||-1<m.indexOf("iPod"))&&e&&534>e||d.operamini&&"[object OperaMini]"==={}.toString.call(d.operamini)||n&&7458>t||-1<f.indexOf("Android")&&e&&533>e||h&&6>h||"palmGetResource"in d&&e&&534>e||-1<f.indexOf("MeeGo")&&-1<f.indexOf("NokiaBrowser/8.5.0")||k&&6>=k)})(jQuery,window);
-(function() {
-
-
-}).call(this);
-(function() {
-
-
-}).call(this);
 // This is a manifest file that'll be compiled into application.js, which will include all the files
 // listed below.
 //
@@ -13798,7 +13694,6 @@ $(document).ready(function(){
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-
 
 
 
